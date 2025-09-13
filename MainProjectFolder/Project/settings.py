e7a464c92bbd68c2299007204ecb762b96593789
dimosojunior <juniordimoso8@gmail.com>
2025-09-13 21:40:23 +0300

    change database

--- MainProjectFolder/Project/settings.py
+++ MainProjectFolder/Project/settings.py
@@ -97,24 +97,24 @@ AUTH_USER_MODEL="App.MyUser"
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'microfinancedb',
-#         'USER':'microfinance',
-#         'PASSWORD':'Dimoso@9898',
-#         'HOST':'localhost',
-#         'PORT':'',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'microfinancedb',
+        'USER':'microfinance',
+        'PASSWORD':'Dimoso@9898',
+        'HOST':'localhost',
+        'PORT':'',
+    }
+}
 
 
 
